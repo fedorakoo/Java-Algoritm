edd78866b1a88096575e6d119fba75d4ddc974a5
fedorakoo <149719081+fedorakoo@users.noreply.github.com>
2024-04-05 13:26:10 +0300

    Update Degue.java

--- src/task_1_3_33/Degue.java
+++ src/task_1_3_33/Degue.java
@@ -1,10 +1,19 @@
 package task_1_3_33;
 
+import java.util.Scanner;
+
 public class Degue<Item>
 {
+    Scanner in = new Scanner(System.in);
     public Degue(int newSize) {
         size = newSize;
         arr = (Item[]) new Object[size];
+    }
+    public void inputArray() {
+        for(int i = 0; i < size; i++) {
+            System.out.print("Введите элемент номер " + Integer.toString(i + 1) + ": ");
+            arr[i] = (Item) in.nextLine();
+        }
     }
     public boolean isEmpty() {
         return (size == 0);
@@ -23,22 +32,30 @@ public class Degue<Item>
         size++;
     }
     public Item popLeft() {
-        Item result = arr[0];
-        arr = (Item[])resizingArrayDeque.getArrSmallerOneFirstLeft(arr, size);
-        size--;
-        return result;
+        if(!isEmpty()) {
+            Item result = arr[0];
+            arr = (Item[]) resizingArrayDeque.getArrSmallerOneFirstLeft(arr, size);
+            size--;
+            return result;
+        }
+        return null;
     }
     public Item popRight() {
-        Item result = arr[size - 1];
-        arr = (Item[])resizingArrayDeque.getArrSmallerOneFirstRight(arr, size);
-        size--;
-        return result;
-    }
-    public void ouputArray() {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        if(!isEmpty()) {
+            Item result = arr[size - 1];
+            arr = (Item[]) resizingArrayDeque.getArrSmallerOneFirstRight(arr, size);
+            size--;
+            return result;
         }
-        System.out.print("\n");
+        return null;
+    }
+    public String ouputArray() {
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+            result +=  arr[i] + " ";
+        }
+        result += '\n';
+        return result;
     }
     private ResizingArrayDeque resizingArrayDeque = new ResizingArrayDeque();
     private Item[] arr;
