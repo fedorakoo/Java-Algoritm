a982d371696d56f824799b4ba91a6efff5f26461
alexander <fedorakooo@gmail.com>
2024-04-06 13:51:35 +0300

    add Main.class, add Degue.java, add ResizingArrayDeque.java

---
+++ src/task_1_3_33/Degue.java
@@ -0,0 +1,64 @@
+package task_1_3_33;
+
+import java.util.Scanner;
+
+public class Degue<Item>
+{
+    Scanner in = new Scanner(System.in);
+    public Degue(int newSize) {
+        size = newSize;
+        arr = (Item[]) new Object[size];
+    }
+    public void inputArray() {
+        for(int i = 0; i < size; i++) {
+            System.out.print("Введите элемент номер " + Integer.toString(i + 1) + ": ");
+            arr[i] = (Item) in.nextLine();
+        }
+    }
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+    public int getSize() {
+        return size;
+    }
+    public void pushLeft(Item item) {
+        arr = (Item[])resizingArrayDeque.getArrLargerOneFirstLeft(arr, size);
+        size++;
+        arr[0] = item;
+    }
+    public void pushRight(Item item) {
+        arr = (Item[])resizingArrayDeque.getArrLargerOneFirstRight(arr, size);
+        arr[size] = item;
+        size++;
+    }
+    public Item popLeft() {
+        if(!isEmpty()) {
+            Item result = arr[0];
+            arr = (Item[]) resizingArrayDeque.getArrSmallerOneFirstLeft(arr, size);
+            size--;
+            return result;
+        }
+        return null;
+    }
+    public Item popRight() {
+        if(!isEmpty()) {
+            Item result = arr[size - 1];
+            arr = (Item[]) resizingArrayDeque.getArrSmallerOneFirstRight(arr, size);
+            size--;
+            return result;
+        }
+        return null;
+    }
+    public String ouputArray() {
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+            result +=  arr[i] + " ";
+        }
+        result+= '\n';
+        return result;
+    }
+    private ResizingArrayDeque resizingArrayDeque = new ResizingArrayDeque();
+    private Item[] arr;
+    private int size;
+}
+
---
+++ src/task_1_3_33/ResizingArrayDeque.java
@@ -0,0 +1,33 @@
+package task_1_3_33;
+
+public class ResizingArrayDeque<Item>
+{
+    public Item[] getArrLargerOneFirstLeft(Item[] arr, int size) {
+        Item[] newArr = (Item[]) new Object[size + 1];
+        for (int i = 1; i < arr.length; i++) {
+            newArr[i] = arr[i - 1];
+        }
+        return newArr;
+    }
+    public Item[] getArrLargerOneFirstRight(Item[] arr, int size) {
+        Item[] newArr = (Item[]) new Object[size + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+    public Item[] getArrSmallerOneFirstLeft(Item[] arr, int size) {
+        Item[] newArr = (Item[]) new Object[size - 1];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[i + 1];
+        }
+        return newArr;
+    }
+    public Item[] getArrSmallerOneFirstRight(Item[] arr, int size) {
+        Item[] newArr = (Item[]) new Object[size - 1];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+}
