0c6240ce4277768c1920efb48e5024778f376746
alexander <fedorakooo@gmail.com>
2024-04-12 20:28:20 +0300

    Add task_2_5_13

---
+++ src/using_sorts/task_2_5_13/LPT.java
@@ -0,0 +1,19 @@
+package using_sorts.task_2_5_13;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Collections;
+public class LPT {
+    private LPT() {
+
+    }
+    public static String orderOfExecution(Map<Double, String> unsortedMap) {
+        StringBuilder bld = new StringBuilder();
+        Map<Double, String> sortedMap = new TreeMap<>(Collections.reverseOrder());
+        sortedMap.putAll(unsortedMap);
+        for (Map.Entry<Double, String> entry : sortedMap.entrySet()) {
+            bld.append("Наименование задания: " + entry.getValue() + "\t|\tКоличество часов: " + entry.getKey() + "\n");
+        }
+        return bld.toString();
+    }
+}
