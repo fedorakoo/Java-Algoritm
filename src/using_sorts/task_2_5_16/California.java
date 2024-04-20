871f5366c9c61fc3f9ceea8914afb3c2a8b8bdd1
Alexander <fedorakooo@gmail.com>
2024-04-21 01:10:16 +0300

    Update task_2_5_16

--- src/using_sorts/task_2_5_16/California.java
+++ src/using_sorts/task_2_5_16/California.java
@@ -6,15 +6,13 @@ import java.util.Scanner;
 public class California {
     public static void main(String[] args) {
         StringBuilder bld = new StringBuilder();
-        System.out.println("Условие задания: Выборы без предпочтений. Чтобы не ущемлять кандидатов, имена которых находятся в конце алфавита, на выборах губернатора Калифорнии в 2003 г. их упорядочили с помощью определенного набора латинских символов. Напишите клиент California с единственным статическим методом main(), который упорядочивает строки в таком порядке. Считайте, что все строки содержат только прописные буквы.");
-        Scanner in = new Scanner(System.in);
+        System.out.println("Условие задания: Выборы без предпочтений. Чтобы не ущемлять кандидатов, имена которых находятся в конце алфавита, на выборах губернатора Калифорнии в 2003 г. их упорядочили с помощью определенного набора латинских символов: {r, w, q, o, j, m, v, a, h, b, s, g, z, x, n, t, c, i, e, k, u, p, d, y, f, l}. Напишите клиент California с единственным статическим методом main(), который упорядочивает строки в таком порядке. Считайте, что все строки содержат только прописные буквы.");
         System.out.print("Введите количество кандидатов: ");
-        int number = in.nextInt();
+        int number = inputInt();
         final int SIZE_ENGLISH_ALPHABET = 26;
         ArrayList<String> candidates = new ArrayList<>();
         for (int i = 0; i < number; i++) {
-            System.out.print("Введите фамилию кандидата номер " + (i + 1) + ". ");
-            candidates.add(in.next());
+            candidates.add(inputName("Введите фамилию кандидата номер " + (i + 1) + ". "));
         }
         int[] arrLetterToSort = {'r', 'w', 'q', 'o', 'j', 'm', 'v', 'a', 'h', 'b', 's', 'g', 'z', 'x', 'n', 't', 'c', 'i', 'e', 'k', 'u', 'p', 'd', 'y', 'f', 'l'};
         int[] letterPower = new int[SIZE_ENGLISH_ALPHABET];
@@ -51,11 +49,44 @@ public class California {
                 }
             }
         }
-
         System.out.println("Конечный список кандидатов:");
         for (int i = 0; i < candidates.size(); i++) {
             bld.append(String.valueOf(i + 1) + "." + candidates.get(i) + "\n");
         }
         System.out.println(bld);
+    }
+    static String inputName(String information) {
+        Scanner in = new Scanner(System.in);
+        boolean isCorrect;
+        String inputName;
+        do {
+            isCorrect = true;
+            System.out.print(information);
+            inputName = in.nextLine();
+            for (int i = 0; i < inputName.length(); i++) {
+                if (!(inputName.charAt(i) >= 'a' && inputName.charAt(i) <= 'z')) {
+                    isCorrect = false;
+                }
+            }
+        } while(!isCorrect);
+        return inputName;
+    }
+    private static int inputInt() {
+        Scanner in = new Scanner(System.in);
+        String number;
+        number = in.nextLine();
+        while (!isNumberInteger(number) || Integer.parseInt(number) < 0) {
+            System.out.print("Введите корректное значение повторно: ");
+            number = in.nextLine();
+        }
+        return Integer.parseInt(number);
+    }
+    private static boolean isNumberInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
