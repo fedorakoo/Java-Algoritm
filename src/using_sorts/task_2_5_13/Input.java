a14710d4dc6369746be4c5f513bb1fa2eb367b47
Alexander Fedorako <fedorakooo@gmail.com>
2024-04-28 18:21:23 +0300

    Update task_2_5_13

---
+++ src/using_sorts/task_2_5_13/Input.java
@@ -0,0 +1,63 @@
+package using_sorts.task_2_5_13;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Input {
+    public static String inputName(Map<String, Double> map) {
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        while (isRepeat(name, map)) {
+            System.out.println(name);
+            System.out.print("Введите корректное значение имени повторно: ");
+            name = in.nextLine();
+        }
+        return name;
+    }
+    public static boolean isRepeat(String name, Map<String, Double> map) {
+        Set<String> keys = map.keySet();
+        for(String key : keys) {
+            if(name.equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int inputInt() {
+        Scanner in = new Scanner(System.in);
+        String number;
+        number = in.nextLine();
+        while (!isNumberInteger(number) || Integer.parseInt(number) < 0) {
+            System.out.print("Введите корректное значение повторно: ");
+            number = in.nextLine();
+        }
+        return Integer.parseInt(number);
+    }
+    public static boolean isNumberInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public static double inputDouble() {
+        Scanner in = new Scanner(System.in);
+        String number;
+        number = in.nextLine();
+        while (!isNumberDouble(number) || Double.parseDouble(number) <= 0) {
+            System.out.print("Введите корректное значение повторно: ");
+            number = in.nextLine();
+        }
+        return Double.parseDouble(number);
+    }
+    public static boolean isNumberDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }  
+}
--- src/using_sorts/task_2_5_13/Main.java
+++ src/using_sorts/task_2_5_13/Main.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
+import static using_sorts.task_2_5_13.Input.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Условие задания: Напишите программу LPT.java, которая принимает из командной строки целое число М, читает из стандартного ввода имена заданий и времена их выполнения и выводит расписание назначения заданий процессорам, которое приблизительно минимизирует время завершения последнего задания с помощью правила “наибольшее время выполнения — сначала”.");
@@ -19,60 +21,5 @@ public class Main {
             unsortedMap.put(name, time);
         }
         System.out.print("\nПорядок выполнения заданий\n" + LPT.orderOfExecution(unsortedMap));
-    }
-    private static String inputName(Map<String, Double> map) {
-        Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        while (isRepeat(name, map)) {
-            System.out.println(name);
-            System.out.print("Введите корректное значение имени повторно: ");
-            name = in.nextLine();
-        }
-        return name;
-    }
-    private static boolean isRepeat(String name, Map<String, Double> map) {
-        Set<String> keys = map.keySet();
-        for(String key : keys) {
-            if(name.equals(key)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private static int inputInt() {
-        Scanner in = new Scanner(System.in);
-        String number;
-        number = in.nextLine();
-        while (!isNumberInteger(number) || Integer.parseInt(number) < 0) {
-            System.out.print("Введите корректное значение повторно: ");
-            number = in.nextLine();
-        }
-        return Integer.parseInt(number);
-    }
-    private static boolean isNumberInteger(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-    private static double inputDouble() {
-        Scanner in = new Scanner(System.in);
-        String number;
-        number = in.nextLine();
-        while (!isNumberDouble(number) || Double.parseDouble(number) <= 0) {
-            System.out.print("Введите корректное значение повторно: ");
-            number = in.nextLine();
-        }
-        return Double.parseDouble(number);
-    }
-    private static boolean isNumberDouble(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
     }
 }
