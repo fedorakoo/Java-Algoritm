cc9f7b41e7c4cdefefa3742d5c22f93605ce7977
Alexander Fedorako <fedorakooo@gmail.com>
2024-05-02 16:25:38 +0300

    Update task_3_2_6

--- src/binary_search_tree/task_3_2_6/Main.java
+++ src/binary_search_tree/task_3_2_6/Main.java
@@ -1,7 +1,7 @@
 package binary_search_tree.task_3_2_6;
 
-import static binary_search_tree.InputCheck.Input.inputInt;
-import static binary_search_tree.InputCheck.Input.inputIntLimit;
+import static binary_search_tree.input.Input.inputInt;
+import static binary_search_tree.input.Input.inputIntLimit;
 
 public class Main {
     public static void main(String[] args) {
