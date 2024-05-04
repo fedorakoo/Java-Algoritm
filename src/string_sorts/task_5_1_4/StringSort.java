09c0f625ee73b38924ee926279b62853276d5e72
Alexander Fedorako <fedorakooo@gmail.com>
2024-05-04 15:42:07 +0300

    Update task_5_1_4

--- src/string_sorts/task_5_1_4/StringSort.java
+++ src/string_sorts/task_5_1_4/StringSort.java
@@ -10,32 +10,32 @@ public class StringSort {
         if (d < s.length()) return s.charAt(d);
         else return -1;
     }
-    public static void quickStringSort(List<String> a) {
-        sort(a, 0, a.size() - 1, 0);
+    public static void quickStringSort(List<String> arr) {
+        sort(arr, 0, arr.size() - 1, 0);
     }
-    private static void sort(List<String> a, int left, int right, int d) {
+    private static void sort(List<String> arr, int left, int right, int d) {
         if (right <= left) return;
         int ptr1 = left;
         int ptr2 = right;
-        int v = charAt(a.get(left), d);
+        int v = charAt(arr.get(left), d);
         int i = left + 1;
         while (i <= ptr2) {
-            int t = charAt(a.get(i), d);
+            int t = charAt(arr.get(i), d);
             if (t < v) {
-                swap(a, ptr1++, i++);
+                swap(arr, ptr1++, i++);
             } else if (t > v) {
-                swap(a, i, ptr2--);
+                swap(arr, i, ptr2--);
             } else {
                 i++;
             }
         }
-        sort(a, left, ptr1 - 1, d);
-        if (v >= 0) sort(a, ptr1, ptr2, d + 1);
-        sort(a, ptr2 + 1, right, d);
+        sort(arr, left, ptr1 - 1, d);
+        if (v >= 0) sort(arr, ptr1, ptr2, d + 1);
+        sort(arr, ptr2 + 1, right, d);
     }
-    private static void swap(List<String> a, int ptr1, int ptr2) {
-        String temp = a.get(ptr1);
-        a.set(ptr1, a.get(ptr2));
-        a.set(ptr2, temp);
+    private static void swap(List<String> arr, int ptr1, int ptr2) {
+        String temp = arr.get(ptr1);
+        arr.set(ptr1, arr.get(ptr2));
+        arr.set(ptr2, temp);
     }
 }
