d2663f351816faac997cfa85d247e0d8a9b03a8b
Alexander Fedorako <fedorakooo@gmail.com>
2024-05-06 14:27:24 +0300

    Update task_3_3_22

--- src/balanced_search_tree/task_3_3_22/RedBlackTree.java
+++ src/balanced_search_tree/task_3_3_22/RedBlackTree.java
@@ -294,8 +294,8 @@ public class RedBlackTree {
         }
     }
     int height(Node node) {
-        if(node == nil) {
-            return 0;
+        if(node == null) {
+            return -1;
         }
         return 1 + Math.max(height(node.left), height(node.right));
     }
