7d9090319a88ac99ffd4e4a6d69926b072f864ed
Alexander Fedorako <fedorakooo@gmail.com>
2024-05-06 21:04:55 +0300

    Update task_5_1_4

--- src/string_sorts/task_5_1_4/StringSort.java
+++ src/string_sorts/task_5_1_4/StringSort.java
@@ -18,22 +18,17 @@ public class StringSort {
         int v = charAt(arr[left], d);
         int i = left + 1;
         while (i <= ptr2) {
-            for(int j = 0; j < arr.length; j++) {
+            for (int j = 0; j < arr.length; j++) {
                 System.out.print(arr[j] + " ");
             }
             System.out.println();
-            if(i > arr.length - 1) {
-                return;
-            }
-            else {
-                int t = charAt(arr[i], d);
-                if (t < v) {
-                    swap(arr, ptr1++, i++);
-                } else if (t > v) {
-                    swap(arr, i, ptr2--);
-                } else {
-                    i++;
-                }
+            int t = (i > arr.length - 1) ? arr.length - 1 : charAt(arr[i], d);
+            if (t < v) {
+                swap(arr, ptr1++, i++);
+            } else if (t > v) {
+                swap(arr, i, ptr2--);
+            } else {
+                i++;
             }
         }
         sort(arr, left, ptr1 - 1, d);
