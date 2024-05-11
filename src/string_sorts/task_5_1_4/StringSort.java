a56c556bab8a4ad5f173e8781b5e7368d0b2a4a4
Alexander Fedorako <fedorakooo@gmail.com>
2024-05-11 23:57:13 +0300

    Update 5_1_4

--- src/string_sorts/task_5_1_4/StringSort.java
+++ src/string_sorts/task_5_1_4/StringSort.java
@@ -1,5 +1,7 @@
 package string_sorts.task_5_1_4;
 
+import java.util.List;
+
 public class StringSort {
     StringSort() {
 
@@ -8,31 +10,38 @@ public class StringSort {
         if (d < s.length()) return s.charAt(d);
         else return -1;
     }
-    public static void quickStringSort(String[] arr) {
-        sort(arr, 0, arr.length - 1, 0);
+    public static void quickStringSort(List<String> arr) {
+        sort(arr, 0, arr.size() - 1, 0);
     }
-    private static void sort(String[] arr, int left, int right, int d) {
+    private static void sort(List<String> arr, int left, int right, int d) {
         if (right <= left) return;
         int ptr1 = left;
         int ptr2 = right;
-        int v = charAt(arr[left], d);
+        int v = charAt(arr.get(left), d);
         int i = left + 1;
         while (i <= ptr2) {
-            for(int j = 0; j < arr.length; j++) {
-                System.out.print(arr[j] + " ");
+            for(int j = 0; j < arr.size(); j++) {
+                System.out.print(arr.get(j) + " ");
             }
             System.out.println();
             String value;
-            if (i < arr.length) {
-                value = arr[i];
+            if (i < arr.size()) {
+                value = arr.get(i);
             } else {
                 return;
             }
             int t = charAt(value, d);
             if (t < v) {
-                swap(arr, ptr1++, i++);
+                String temp = arr.get(ptr1);
+                arr.set(ptr1, arr.get(i));
+                arr.set(i, temp);
+                ptr1++;
+                i++;
             } else if (t > v) {
-                swap(arr, i, ptr2--);
+                String temp = arr.get(ptr2);
+                arr.set(ptr2, arr.get(i));
+                arr.set(i, temp);
+                ptr2--;
             } else {
                 i++;
             }
@@ -40,10 +49,5 @@ public class StringSort {
         sort(arr, left, ptr1 - 1, d);
         if (v >= 0) sort(arr, ptr1, ptr2, d + 1);
         sort(arr, ptr2 + 1, right, d);
-    }
-    private static void swap(String[] arr, int ptr1, int ptr2) {
-        String temp = arr[ptr1];
-        arr[ptr1] =  arr[ptr2];
-        arr[ptr2] =  temp;
     }
 }
