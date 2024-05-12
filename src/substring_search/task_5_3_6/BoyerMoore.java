bf1cc4917a2a9769331ba71330d257c29ea3e372
Alexander Fedorako <fedorakooo@gmail.com>
2024-05-12 14:37:16 +0300

    Add task_5_3_6

---
+++ src/substring_search/task_5_3_6/BoyerMoore.java
@@ -0,0 +1,21 @@
+package substring_search.task_5_3_6;
+public class BoyerMoore {
+    private int[] right;
+
+    BoyerMoore(String pat) {
+        int m = pat.length();
+        int r = 256;
+        right = new int[r];
+
+        for (int c = 0; c < r; c++) {
+            right[c] = -1;
+        }
+        for (int j = 0; j < m; j++) {
+            right[pat.charAt(j)] = j;
+        }
+        System.out.print("Содержимое массива right: ");
+        for(int i = 0; i < right.length; i++) {
+            System.out.print(right[i] + " ");
+        }
+    }
+}
---
+++ src/substring_search/task_5_3_6/Main.java
@@ -0,0 +1,9 @@
+package substring_search.task_5_3_6;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Условие задания: Приведите содержимое массива right[], вычисленного конструктором в алгоритме Бойера-Мура для образца ABRACADABRA.");
+        String condition = "ABRACADABRA";
+        new BoyerMoore(condition);
+    }
+}
